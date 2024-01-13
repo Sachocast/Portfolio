dce82bd5483877b0fb16ea5fecdba04ac2ac2f0e
castillejos sacha <sacha.castillejos@etu.unice.fr>
2024-01-13 01:31:12 +0100

    slider suite

--- www/js/index.js
+++ www/js/index.js
@@ -1,27 +1,71 @@
 let currentImageIndex = 0;
-const images = [
+const imagesAttMstr = [
     "./img/attaqueDeMstr1.png",
     "./img/attaqueDeMstr2.png",
     "./img/attaqueDeMstr3.png",
     "./img/attaqueDeMstr4.png"
 ];
-const currentImageElement = document.getElementById("currentImage");
+const imagesPkmn = [
+    "./img/pkmn1.png",
+    "./img/pkmn2.png",
+    "./img/pkmn3.png"
+]
+var currentImageElement = 0;
 
-function updateImage() {
-    currentImageElement.src = images[currentImageIndex];
+function updateImage(idDiv) {
+    switch(idDiv){
+        case 'attMstr':
+            currentImageElement.src = imagesAttMstr[currentImageIndex];
+            break;
+        case 'pkmn' :
+            currentImageElement.src = imagesPkmn[currentImageIndex];
+    }
+}
+    
+
+function nextImage(idDiv) {
+    removeCurrentImageId();
+    addCurrentImageId(idDiv);
+    currentImageElement = document.getElementById("currentImage");
+    switch(idDiv){
+        case 'attMstr':
+            currentImageIndex = (currentImageIndex + 1) % imagesAttMstr.length;
+            break;
+        case 'pkmn' :
+            currentImageIndex = (currentImageIndex + 1) % imagesPkmn.length;
+    }
+    updateImage(idDiv);
 }
 
-function nextImage() {
-    currentImageIndex = (currentImageIndex + 1) % images.length;
-    console.log(currentImageIndex);
-    updateImage();
+function prevImage(idDiv) {
+    removeCurrentImageId();
+    addCurrentImageId(idDiv);
+    currentImageElement = document.getElementById("currentImage");
+    switch(idDiv){
+        case 'attMstr':
+            currentImageIndex = (currentImageIndex + 1) % imagesAttMstr.length;
+            break;
+        case 'pkmn' :
+            currentImageIndex = (currentImageIndex + 1) % imagesPkmn.length;
+    }
+    updateImage(idDiv);
 }
 
-function prevImage() {
-    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-    updateImage();
+function removeCurrentImageId() {
+    const images = document.querySelectorAll('.slider-image');
+
+    images.forEach((image) => {
+        image.removeAttribute('id');
+    });
 }
 
-function zoom(element) {
-    element.classList.toggle("fullsize");
+function addCurrentImageId(idDiv) {
+    const div = document.getElementById(idDiv);
+
+    if (div) {
+        const imgElement = div.querySelector('.slider-image');
+        if (imgElement) {
+            imgElement.id = "currentImage";
+        }
+    }
 }
